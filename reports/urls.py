84aec7754b246a4e33e80676fcfc8dabbfce33e2
mwandi <frankmwandi93@gmail.com>
2024-06-04 19:52:07 +0300

    urls

--- reports/urls.py
+++ reports/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
+from . import views
 
 urlpatterns = [
-    #path('/'),
+    path('', views.home, name='home'),
+    path('add_claim/', views.add_claim, name='add_claim'),
+    path('update_progress/<int:claim_id>/', views.update_progress, name='update_progress'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
