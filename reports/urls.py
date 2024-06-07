70e69bd9499c56709ea53072d659def9ee518c4f
mwandi <frankmwandi93@gmail.com>
2024-06-07 19:09:50 +0300

    updated urls

--- reports/urls.py
+++ reports/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from .views import ClaimCreateView, ClaimDetailView, ProgressAddView, ClaimStatsView, AccuserCreateView, AccusedCreateView
+from .views import ClaimCreateView, ClaimDetailView, ProgressAddView, ClaimStatsView, search_claims, claim_stats_pdf, AccuserCreateView, AccusedCreateView
 
 urlpatterns = [
     path('create/', ClaimCreateView.as_view(), name='create_claim'),
     path('<int:claim_id>/', ClaimDetailView.as_view(), name='claim_detail'),
     path('<int:claim_id>/add_progress/', ProgressAddView.as_view(), name='add_progress'),
     path('stats/', ClaimStatsView.as_view(), name='claim_stats'),
+    path('search/', search_claims, name='search_claims'),
+    path('stats/pdf/', claim_stats_pdf, name='claim_stats_pdf'),
     path('add_accuser/', AccuserCreateView.as_view(), name='add_accuser'),
     path('add_accused/', AccusedCreateView.as_view(), name='add_accused'),
 ]
